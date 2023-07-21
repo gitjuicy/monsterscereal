2ff2cee8386199ebec80b1c63b675b206cc6c727
Alexander Mathis <alexander@juicyorange.com>
2023-07-21 12:10:11 -0400

    More changes
    - Added concurrently
    - Started work on product page
    - Fixed tailwind

--- assets/product-form.js
+++ assets/product-form.js
@@ -8,12 +8,9 @@ if (!customElements.get('product-form')) {
         this.form = this.querySelector('form');
         this.form.querySelector('[name=id]').disabled = false;
         this.form.addEventListener('submit', this.onSubmitHandler.bind(this));
-        this.cart =
-          document.querySelector('cart-notification') ||
-          document.querySelector('cart-drawer');
+        this.cart = document.querySelector('cart-notification') || document.querySelector('cart-drawer');
         this.submitButton = this.querySelector('[type="submit"]');
-        if (document.querySelector('cart-drawer'))
-          this.submitButton.setAttribute('aria-haspopup', 'dialog');
+        if (document.querySelector('cart-drawer')) this.submitButton.setAttribute('aria-haspopup', 'dialog');
 
         this.hideErrors = this.dataset.hideErrors === 'true';
       }
@@ -26,9 +23,7 @@ if (!customElements.get('product-form')) {
 
         this.submitButton.setAttribute('aria-disabled', true);
         this.submitButton.classList.add('loading');
-        this.querySelector('.loading-overlay__spinner').classList.remove(
-          'hidden',
-        );
+        this.querySelector('.loading-overlay__spinner').classList.remove('hidden');
 
         const config = fetchConfig('javascript');
         config.headers['X-Requested-With'] = 'XMLHttpRequest';
@@ -38,7 +33,7 @@ if (!customElements.get('product-form')) {
         if (this.cart) {
           formData.append(
             'sections',
-            this.cart.getSectionsToRender().map((section) => section.id),
+            this.cart.getSectionsToRender().map((section) => section.id)
           );
           formData.append('sections_url', window.location.pathname);
           this.cart.setActiveElement(document.activeElement);
@@ -57,8 +52,7 @@ if (!customElements.get('product-form')) {
               });
               this.handleErrorMessage(response.description);
 
-              const soldOutMessage =
-                this.submitButton.querySelector('.sold-out-message');
+              const soldOutMessage = this.submitButton.querySelector('.sold-out-message');
               if (!soldOutMessage) return;
               this.submitButton.setAttribute('aria-disabled', true);
               this.submitButton.querySelector('span').classList.add('hidden');
@@ -85,7 +79,7 @@ if (!customElements.get('product-form')) {
                     this.cart.renderContents(response);
                   });
                 },
-                { once: true },
+                { once: true }
               );
               quickAddModal.hide(true);
             } else {
@@ -97,25 +91,17 @@ if (!customElements.get('product-form')) {
           })
           .finally(() => {
             this.submitButton.classList.remove('loading');
-            if (this.cart && this.cart.classList.contains('is-empty'))
-              this.cart.classList.remove('is-empty');
+            if (this.cart && this.cart.classList.contains('is-empty')) this.cart.classList.remove('is-empty');
             if (!this.error) this.submitButton.removeAttribute('aria-disabled');
-            this.querySelector('.loading-overlay__spinner').classList.add(
-              'hidden',
-            );
+            this.querySelector('.loading-overlay__spinner').classList.add('hidden');
           });
       }
 
       handleErrorMessage(errorMessage = false) {
         this.errorMessageWrapper =
-          this.errorMessageWrapper ||
-          this.querySelector('.product-form__error-message-wrapper');
+          this.errorMessageWrapper || this.querySelector('.product-form__error-message-wrapper');
         if (!this.errorMessageWrapper) return;
-        this.errorMessage =
-          this.errorMessage ||
-          this.errorMessageWrapper.querySelector(
-            '.product-form__error-message',
-          );
+        this.errorMessage = this.errorMessage || this.errorMessageWrapper.querySelector('.product-form__error-message');
 
         this.errorMessageWrapper.toggleAttribute('hidden', !errorMessage);
 
@@ -123,6 +109,6 @@ if (!customElements.get('product-form')) {
           this.errorMessage.textContent = errorMessage;
         }
       }
-    },
+    }
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,28 +1,52 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  prefix: 'twcss-',
-  content: [
-    './layout/*.liquid',
-    './templates/*.liquid',
-    './templates/customers/*.liquid',
-    './sections/*.liquid',
-    './snippets/*.liquid',
-  ],
+  content: ['./app/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    screens: {
-      sm: '320px',
-      md: '750px',
-      lg: '990px',
-      xlg: '1440px',
-      x2lg: '1920px',
-      pageMaxWidth: '1440px',
-    },
     extend: {
-      fontFamily: {
-        heading: 'var(--font-heading-family)',
+      colors: {
+        black: {
+          light: '#1b1b1b',
+          DEFAULT: '#000000',
+        },
+        blue: {
+          light: '#45a3e6',
+          DEFAULT: '#1689ca',
+        },
+        brown: {
+          light: '#be663b',
+          DEFAULT: '#a04d24',
+        },
+        green: {
+          light: '#c2e956',
+          DEFAULT: '#a5cd39',
+        },
+        pink: {
+          light: '#ff3f8c',
+          DEFAULT: '#ed0973',
+        },
+        purple: {
+          DEFAULT: '#462772',
+        },
+        red: {
+          light: '#c3273c',
+          DEFAULT: '#aa2234',
+        },
       },
-    },
-    color: {
-      black: '#000000',
+      fontFamily: {
+        coop: 'CoopBold',
+        helvetica: 'Helvetica Neue',
+      },
+      maxWidth: {
+        200: '200px',
+        240: '240px',
+        310: '310px',
+        800: '800px',
+        1000: '1000px',
+        1340: '1340px',
+      },
+      minWidth: {
+        200: '200px',
+      },
     },
   },
   plugins: [],
