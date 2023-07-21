7e28ca05fb6f607072353fdc7d002c41efcc4520
Alexander Mathis <alexander@juicyorange.com>
2023-07-21 12:38:39 -0400

    Fixed tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./app/**/*.{js,ts,jsx,tsx}'],
+  content: ['./**/*.{js,ts,jsx,tsx,liquid}'],
   theme: {
     extend: {
       colors: {
